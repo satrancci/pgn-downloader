5a19c5f78c1366415b9ecd4e1b6cd8a3a11fb523
satrancci <thescarface1993@gmail.com>
2021-01-28 20:02:59 -0600

    display more profile info

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,24 @@ import SearchBar from './SearchBar';
 import chesscom from '../apis/chesscom';
 
 class App extends React.Component {
-  state = { profile: ''};
+  state = {username: null, url: null, followers: null, avatar: null};
 
-  // we will pass it as a prop to the SearchBar component
-  onSearchSubmit = async username => {
-    const response = await chesscom.get(`${username}`);
+  // we will pass onSearchSubmit as a prop to the SearchBar component
+  onSearchSubmit = async usernameSearchTerm => {
+    const response = await chesscom.get(`${usernameSearchTerm}`);
+    const {username, url, followers, avatar} = response.data; // object destructuring
 
-    this.setState({ profile: response.data });
+    this.setState({username, url, followers, avatar}); // a more concise notation for username:username, url:url, etc
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Retrieved: {JSON.stringify(this.state.profile)} 
+        <p><img src={this.state.avatar} /> </p>
+        <p><b>{this.state.username? this.state.username : null} </b></p>
+        <p>{this.state.followers ? `Followers: ${this.state.followers}` : null} </p>
+        <p>{this.state.url ? <a href={this.state.url}> Chess.com Profile </a> : null} </p>
       </div>
     );
   }
