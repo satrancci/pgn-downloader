4f91735519e994ca72650771088f213c3d193a82
satrancci <thescarface1993@gmail.com>
2021-02-08 00:32:25 -0600

    change concurrency setting to 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import { Promise } from 'bluebird';
+import { Promise } from "bluebird";
 
 import DownloadButton from "./DownloadButton";
 import Form from "./Form";
-import Spinner from './Spinner';
+import Spinner from "./Spinner";
 import axios from "axios";
 import chesscom from "../apis/chesscom";
-
-
 
 const checkUserExists = async (username) => {
   const playerExistsResponse = await chesscom.get(`/${username}`);
@@ -19,18 +17,17 @@ const fetchGamesArchive = async (username) => {
   return response.data.archives;
 };
 
-
 const App = () => {
-
   const [games, setGames] = useState([]);
-  const [username, setUsername] = useState('');
-  const [timeControl, setTimeControl] = useState('');
+  const [username, setUsername] = useState("");
+  const [timeControl, setTimeControl] = useState("");
   const [downloading, setDownloading] = useState(false);
 
-
-  const fetchGames = async(values, archives) => {
+  const fetchGames = async (values, archives) => {
     setDownloading(true);
-    const responses = await Promise.map(archives, url => axios.get(url), {concurrency: 2} ); // Chess.com API allows three concurrent requests per each IP address
+    const responses = await Promise.map(archives, (url) => axios.get(url), {
+      concurrency: 1,
+    }); // Chess.com API allows three concurrent requests per each IP address
     const monthly_games = responses.map((res) => res.data.games);
     const concatGames = Object.values(
       monthly_games
@@ -38,12 +35,12 @@ const App = () => {
           month.filter((game) => game.time_class === `${values.timeControl}`)
         )
         .flat()
-      );
-      setGames(concatGames);
-      setUsername(values.username);
-      setTimeControl(values.timeControl);
-      setDownloading(false);
-  }
+    );
+    setGames(concatGames);
+    setUsername(values.username);
+    setTimeControl(values.timeControl);
+    setDownloading(false);
+  };
 
   const interactWithChessComApi = async (values) => {
     checkUserExists(values.username)
@@ -51,12 +48,12 @@ const App = () => {
         fetchGamesArchive(values.username)
           .then((archives) => {
             fetchGames(values, archives)
-            .then(() => {
-              console.log("Games fetched!");
-            })
-            .catch((e) => {
-              console.log("Could not fetch the games", e);
-            });
+              .then(() => {
+                console.log("Games fetched!");
+              })
+              .catch((e) => {
+                console.log("Could not fetch the games", e);
+              });
           })
           .catch((e) => {
             console.log("Could not fetch the archive", e);
@@ -66,14 +63,19 @@ const App = () => {
         alert("The username that you specified is invalid!");
       });
   };
-  
-  
+
   return (
     <div className="ui container">
       <Form onSubmit={interactWithChessComApi} />
       <div>
-      {games.length > 0 && <DownloadButton username={username} timeControl={timeControl} games={games} />}
-      {downloading && <Spinner/>}
+        {games.length > 0 && (
+          <DownloadButton
+            username={username}
+            timeControl={timeControl}
+            games={games}
+          />
+        )}
+        {downloading && <Spinner />}
       </div>
     </div>
   );
